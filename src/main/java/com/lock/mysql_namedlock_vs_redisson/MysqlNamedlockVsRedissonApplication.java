3eb833e26970994eb975b98ae89d18cd7322e8c0
HanYoonSoo <hdbstn3055@gmail.com>
2026-05-11 16:47:24 +0900

    feat: Mysql namedlock 분산락 커넥션 풀과 애플리케이션에서 주 활용 커넥션 풀 분리 작업

--- src/main/java/com/lock/mysql_namedlock_vs_redisson/MysqlNamedlockVsRedissonApplication.java
+++ src/main/java/com/lock/mysql_namedlock_vs_redisson/MysqlNamedlockVsRedissonApplication.java
@@ -1,7 +1,13 @@
 package com.lock.mysql_namedlock_vs_redisson;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class MysqlNamedlockVsRedissonApplication {
@@ -10,4 +16,25 @@ public class MysqlNamedlockVsRedissonApplication {
         SpringApplication.run(MysqlNamedlockVsRedissonApplication.class, args);
     }
 
+    @Primary
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource dataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @ConfigurationProperties("userlock.datasource.hikari")
+    public HikariDataSource userLockDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            flyway.clean();
+            flyway.baseline();
+            flyway.migrate();
+        };
+    }
 }
