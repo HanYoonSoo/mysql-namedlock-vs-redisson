498b7bb531862050cfa2bcf8525709ccfd2e7428
HanYoonSoo <hdbstn3055@gmail.com>
2026-05-11 16:09:33 +0900

    init: 기본 프로젝트 생성 파일 생성

---
+++ src/main/java/com/lock/mysql_namedlock_vs_redisson/MysqlNamedlockVsRedissonApplication.java
@@ -0,0 +1,13 @@
+package com.lock.mysql_namedlock_vs_redisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlNamedlockVsRedissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlNamedlockVsRedissonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lock/mysql_namedlock_vs_redisson/MysqlNamedlockVsRedissonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lock.mysql_namedlock_vs_redisson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlNamedlockVsRedissonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
